fa18c0c20449a2dce53cf720747a82e3607acab0
landchootl <ludovic.landschoot.partner@decathlon.com>
2020-04-29 17:34:28 +0200

    chore (init): import correction 1

---
+++ src/components/ManageStore.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h3>Administration du magasin :</h3>
+    <p><button @click="openOrCloseStore()">{{store.isOpen ? 'Fermer le magasin' : 'Ouvrir le magasin'}}</button></p>
+    <p>Nom du magasin :  <input type="text" v-model="store.name"/></p>
+    <p>Magasin ouvert :  <input type="checkbox" v-model="store.isOpen"/></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ManageStore',
+  props: {
+    store: Object
+  },
+  model: {
+    prop: 'store',
+    event: 'storeChange'
+  },
+  methods: {
+    openOrCloseStore() {
+      this.store.isOpen = !this.store.isOpen;
+      this.$emit('storeChange', this.store);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Store.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <img src="https://img.pngio.com/shop-icon-png-175739-free-icons-library-store-icon-png-1024_1024.jpg" id="img-store"/>
+    <h1>{{fullName}}</h1>
+<!--    <h2 class="open" v-if="store.isOpen">Magasin : open</h2>-->
+<!--    <h2 class="close" v-else>Magasin : close</h2>-->
+    <h2 v-bind:class="{ 'open': store.isOpen, 'close': !store.isOpen }">Magasin : {{store.isOpen ? 'Open' : 'Close'}}</h2>
+    <ManageStore v-model="store"></ManageStore>
+  </div>
+</template>
+
+<script>
+import ManageStore from "./ManageStore";
+
+export default {
+  name: 'Store',
+  components: {
+    ManageStore
+  },
+  data() {
+    return {
+      store: {
+        name: 'Fred',
+        isOpen: false
+      }
+    }
+  },
+  computed: {
+    fullName() {
+      return `Magasin de ${this.store.name}`;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.open {
+  color: green;
+}
+.close {
+  color: red;
+}
+#img-store {
+  width: 30%;
+}
+</style>
